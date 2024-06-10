69c436e901d7045ddfb49ae61f850501435b491c
m.bayzhigit <m.bayzhigit@alterapp.net>
2024-06-10 17:05:35 +0500

    first commit

---
+++ src/app/models/response/track-models.ts
@@ -0,0 +1,21 @@
+import {User} from "../user";
+
+export interface UsersTrack {
+  _id: string,
+  createdDate: Date,
+  description: string,
+  ownerId: User,
+  track: Track,
+}
+export interface Track {
+  _id: string,
+  trackNumber: string,
+  createdBy: string,
+  createdDate: Date,
+  fromChinaToAlmaty: Date,
+  receivedInAlmatyDate: Date,
+  shippedFromAlmatyDate: Date,
+  shippedCity: string,
+  receivedInChinaDate: Date,
+  receivedByClient: Date
+}
